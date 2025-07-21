d579d16d2c826ed5c79aec9f8feb905137767a1a
MURINDABIGWI TRIBERT <tribertmuto@gmail.com>
2025-07-21 13:27:17 +0200

    change

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,13 +1,11 @@
-import React from "react";
-import Button from "@/components/common/Button";
-
-const UsersPage: React.FC = () => {
-  return (
-    <div className="p-6">
-      <h1 className="text-3xl mb-4">Users</h1>
-      <Button title="Add User" />
-    </div>
-  );
-};
-
-export default UsersPage;
+export async function getStaticProps() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+    const posts = await response.json()
+  
+    return {
+      props: {
+        posts
+      }
+    }
+  }
+  
